bbbae28fa82f73b202650f674f03dccecbf0d9ab
Your Name <you@example.com>
2024-11-16 15:13:24 +0530

    Updates

---
+++ precache-manifest.f18febb01101436eec3f9cbf94dee9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9337a1ebccaadf300dd3015b16d126",
+    "url": "/Aswanthkumar486/React-foodRecipe/index.html"
+  },
+  {
+    "revision": "0f018ec6ca0ceded0519",
+    "url": "/Aswanthkumar486/React-foodRecipe/static/css/main.936a2bff.chunk.css"
+  },
+  {
+    "revision": "c9cc1e063dea188b1ae5",
+    "url": "/Aswanthkumar486/React-foodRecipe/static/js/2.0b9c3b66.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Aswanthkumar486/React-foodRecipe/static/js/2.0b9c3b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46bf6fe8c7fad5457fe7",
+    "url": "/Aswanthkumar486/React-foodRecipe/static/js/3.de732d37.chunk.js"
+  },
+  {
+    "revision": "0f018ec6ca0ceded0519",
+    "url": "/Aswanthkumar486/React-foodRecipe/static/js/main.6810fe37.chunk.js"
+  },
+  {
+    "revision": "3c3d14c0576497ff71a6",
+    "url": "/Aswanthkumar486/React-foodRecipe/static/js/runtime-main.785d8d31.js"
+  }
+]);
